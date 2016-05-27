41e9e1eae4c6c534e29ad9eefb7346df42c805c2
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-05-27 20:00:27 +0200

    Update drawing code to be compatible with GRAL 0.11.

--- src/main/java/cec/input/draw/DataDraw.java
+++ src/main/java/cec/input/draw/DataDraw.java
@@ -1,29 +1,45 @@
 package cec.input.draw;
 
+import cec.cluster.Cluster;
 import cec.run.CECAtomic;
+import de.erichseifert.gral.data.DataSource;
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.graphics.Insets2D;
+import de.erichseifert.gral.graphics.Label;
+import de.erichseifert.gral.plots.XYPlot;
+import de.erichseifert.gral.plots.lines.LineRenderer;
+import de.erichseifert.gral.plots.lines.SmoothLineRenderer2D;
+import de.erichseifert.gral.plots.points.DefaultPointRenderer2D;
+import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.ui.InteractivePanel;
+import de.erichseifert.gral.util.GraphicsUtils;
 import tools.ColorGenerator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 /**
- *
  * @author Krzysztof
  */
 public class DataDraw extends JFrame {
 
-    public DataDraw(CECAtomic data) {
+    private CECAtomic data;
+    private Color[] colors = {
+            Color.red, Color.green, Color.gray,
+            Color.magenta, Color.blue, Color.pink,
+            Color.cyan, Color.orange, Color.yellow
+    };
 
-        Color[] colors = {Color.red, Color.green, Color.gray,
-                Color.magenta, Color.blue, Color.pink,
-                Color.cyan, Color.orange, Color.yellow};
+    public DataDraw(CECAtomic data) {
+        this.data = data;
         if (data.getNumberOfClusters() > colors.length) {
             colors = ColorGenerator.randomColorArray(data.getNumberOfClusters());
         }
         LookAndFeel.doIt();
     }
 
-    public void disp() {/*
+    public void disp() {
         if (data.getDimension() != 2) {
             throw new RuntimeException("You can see only 2D data");
         }
@@ -50,13 +66,13 @@ public class DataDraw extends JFrame {
             PointRenderer points1 = new DefaultPointRenderer2D();
             points1.setShape(new Ellipse2D.Double(-3.0, -3.0, 6.0, 6.0));
             points1.setColor(colors[i]);
-            plot.setPointRenderer(s, points1);
+            plot.setPointRenderers(s, points1);
         }
 
         LineRenderer line2 = new SmoothLineRenderer2D();
         line2.setColor(GraphicsUtils.deriveWithAlpha(Color.black, 180));
         line2.setStroke(new BasicStroke(6));
-        plot.setLineRenderer(dt[k + 1], line2);
+        plot.setLineRenderers(dt[k + 1], line2);
 
         data.getClusters().stream().filter((c) -> (!c.isEmpty())).map((c) -> c.getMean()).forEach((m) -> {
             dt[k].add(m.get(0, 0), m.get(1, 0));
@@ -69,7 +85,7 @@ public class DataDraw extends JFrame {
             }
             Ellipse e = new Ellipse(c);
             e.addData(dt[i]);
-            plot.setLineRenderer(dt[i], line2);
+            plot.setLineRenderers(dt[i], line2);
         }
 
         DataSource s = plot.get(k);
@@ -78,7 +94,7 @@ public class DataDraw extends JFrame {
         PointRenderer points1 = new DefaultPointRenderer2D();
         points1.setShape(new Ellipse2D.Double(-10.0, -10.0, 20.0, 20.0));
         points1.setColor(Color.BLACK);
-        plot.setPointRenderer(s, points1);
+        plot.setPointRenderers(s, points1);
 
         // Style the plot
         double insetsTop = 20.0,
@@ -94,8 +110,8 @@ public class DataDraw extends JFrame {
         plot.getPlotArea().setBorderStroke(new BasicStroke(2f));
 
         // Style axes
-        plot.getAxisRenderer(XYPlot.AXIS_X).setLabel("X");
-        plot.getAxisRenderer(XYPlot.AXIS_Y).setLabel("Y");
+        plot.getAxisRenderer(XYPlot.AXIS_X).setLabel(new Label("X"));
+        plot.getAxisRenderer(XYPlot.AXIS_Y).setLabel(new Label("Y"));
         plot.getAxisRenderer(XYPlot.AXIS_X).setTickSpacing(1.0);
         plot.getAxisRenderer(XYPlot.AXIS_Y).setTickSpacing(2.0);
         plot.getAxisRenderer(XYPlot.AXIS_X).setIntersection(-Double.MAX_VALUE);
@@ -106,7 +122,6 @@ public class DataDraw extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setMinimumSize(getContentPane().getMinimumSize());
         this.setSize(550, 550);
-        this.setVisible(true);*/
+        this.setVisible(true);
     }
-
 }
