a051367e12ef1926a7b11c7771867a373a917c52
jkordas <jan.kordas@uj.edu.pl>
2016-05-29 20:16:57 +0200

    Utils class removed

--- src/test/java/accuracy/ComparisonWithR.java
+++ src/test/java/accuracy/ComparisonWithR.java
@@ -5,6 +5,7 @@ import cec.cluster.Cluster;
 import cec.cluster.types.ClusterKind;
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.NormOps;
+import org.ejml.simple.SimpleMatrix;
 import org.junit.Before;
 import org.junit.Rule;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import utils.RetryRule;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -66,7 +68,7 @@ public abstract class ComparisonWithR {
         assertEquals(centers.length, cec.getResult().getClusters().stream().filter(c -> !c.isEmpty()).count());
 
         //create appropriate centers mapping
-        Map<Integer, Integer> clustersActualToExpectedMapping = Utils.findMapping(clusters, centers);
+        Map<Integer, Integer> clustersActualToExpectedMapping = findMapping(clusters, centers);
         assertEquals(centers.length, clustersActualToExpectedMapping.size());
 
 
@@ -84,5 +86,34 @@ public abstract class ComparisonWithR {
             assertThat("Difference", diff, lessThan(EPSILON));
         }
 
+    }
+
+    private static Map<Integer, Integer> findMapping(List<Cluster> clusters, double[][] centers) {
+
+        Map<Integer, Integer> clustersActualToExpectedMapping = new HashMap<>();
+
+        //identify centres
+        int num = 0;
+
+        for (Cluster cluster : clusters) {
+            int mappingNumber = -1;
+            double distance = Double.MAX_VALUE;
+
+            for (int j = 0; j < centers.length; j++) {
+                double[] center = centers[j];
+                SimpleMatrix centerMatrix = new SimpleMatrix(center.length,1,true,center);
+                SimpleMatrix result = centerMatrix.minus(cluster.getMean());
+                result = result.transpose().mult(result);
+
+                if(result.get(0) < distance) {
+                    distance = result.get(0);
+                    mappingNumber = j;
+                }
+            }
+
+            clustersActualToExpectedMapping.put(num++, mappingNumber);
+        }
+
+        return clustersActualToExpectedMapping;
     }
 }
