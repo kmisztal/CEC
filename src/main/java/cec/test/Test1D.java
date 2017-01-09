ed10ddb6431e46309b0c74f55dbfcfbfe6c7a6cf
chrispi <krzysztof.misztal@gmail.com>
2017-01-09 11:53:10 +0100

    test for 1d data

--- src/main/java/cec/test/Test1D.java
+++ src/main/java/cec/test/Test1D.java
@@ -2,9 +2,6 @@ package cec.test;
 
 import cec.CEC;
 import cec.cluster.types.ClusterKind;
-import stats.AICScore;
-import stats.BICScore;
-import stats.LogMLEScore;
 
 import java.io.IOException;
 
@@ -15,33 +12,11 @@ public class Test1D {
     public static void main(String[] args) throws IOException {
         CEC cec = new CEC();
 
-        // to pass command line arguments
-        // these arguments overwrite other default/file configuration
-        //CEC cec = new CEC(args);
-
         String src = "src/main/resources/data_test/csv/gauss3x_in1d.csv";
         cec.setData(src,
                 "text/space-separated-values");
 
         cec.add(ClusterKind.Gaussians, 30);
-//
-//        cec.add(ClusterKind.LambdaGaussians, 3,
-//                TypeOption.add("lambda", new double[]{1., 0.1}));
-//
-//        cec.add(ClusterKind.CovarianceGaussians, 3,
-//                TypeOption.add("covariance", new double[][]{{1., 0.1}, {0.1, 1}}));
-//
-//        cec.add(ClusterKind.DeterminantGaussians, 4,
-//                TypeOption.add("det", 0.1)
-//        );
-//
-//        cec.add(ClusterKind.DiagonalGaussians, 3);
-
-//        cec.add(ClusterKind.SphericalGaussians, 30);
-
-//        cec.add(ClusterKind.SphericalGaussiansWithFixedRadius, 3,
-//                TypeOption.add("r", 0.5)
-//        );
 
         cec.run();
 
@@ -54,13 +29,10 @@ public class Test1D {
 
 //        new DataDraw(cec.getResult()).disp();
 
-        //        System.out.println(cec.getResult().getPartition());
-        cec.getResult().savePartition(src + ".part");
-
-
-        //        System.out.println("logMLE = " + LogLikelihoodFunction.loglikelihood(cec.getResult()));
-        System.out.println("logMLE = " + new LogMLEScore().score(cec.getResult()));
-        System.out.println("AIC = " + new AICScore().score(cec.getResult()));
-        System.out.println("BIC = " + new BICScore().score(cec.getResult()));
+//        cec.getResult().savePartition(src + ".part");
+//        System.out.println("logMLE = " + LogLikelihoodFunction.loglikelihood(cec.getResult()));
+//        System.out.println("logMLE = " + new LogMLEScore().score(cec.getResult()));
+//        System.out.println("AIC = " + new AICScore().score(cec.getResult()));
+//        System.out.println("BIC = " + new BICScore().score(cec.getResult()));
     }
 }
